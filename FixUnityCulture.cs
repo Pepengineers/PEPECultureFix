de25129ec3bfe0b36c8ebe4242451fe37cc48dc2
Mikhanil <raeinforce@gmail.com>
2025-04-30 21:40:13 +0300

    fix namespaces

--- FixUnityCulture.cs
+++ FixUnityCulture.cs
@@ -1,9 +1,11 @@
 ﻿using System.Globalization;
 using System.Threading;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
-namespace UnityPatches
+namespace Pepengineers
 {
 #if UNITY_EDITOR
 	[InitializeOnLoad]
